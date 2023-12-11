dd21114d67e0fc720a8c63c05f60fd95aa0cfdb6
Marco Ammann <marco.ammann@hotmail.com>
2023-12-11 23:34:02 +0100

    minor changes

--- js/add_task.js
+++ js/add_task.js
@@ -1,5 +1,9 @@
-
-
+const contacts = [
+    { id: 1, name: "Ammann Marco" },
+    { id: 2, name: "Joost Heidrich" },
+    { id: 3, name: "Ghulammustafa Kakar" },
+    { id: 4, name: "Sebastian Treittinger" }
+  ];
 
 function openAssignDropdown(){
     
