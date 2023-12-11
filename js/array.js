586e1aea95c4dc61d8598fca2bd976c07bcadfb4
joostheidrich <joost.heidrich@web.de>
2023-12-11 19:43:40 +0100

    array finished

--- js/array.js
+++ js/array.js
@@ -1,26 +1,22 @@
-let tasks = [{
-    "taskTitle": "Titel des Tasks",
-    "description": "Beschreibung des Tasks",
-    "assignedTo": ["Kontakt1", "Kontakt2", "Kontakt3"],
-    "dueDate": "Fälligkeitsdatum",
-    "priority": "Urgent/Medium/Low",
-    "category": "Kategorie",
-    "subtasks": ["Subtask1", "Subtask2", "Subtask3"]
-  },
-  {
-    "taskTitle": "Titel des Tasks",
-    "description": "Beschreibung des Tasks",
-    "assignedTo": ["Kontakt1", "Kontakt2", "Kontakt3"],
-    "dueDate": "Fälligkeitsdatum",
-    "priority": "Urgent/Medium/Low",
-    "category": "Kategorie",
-    "subtasks": ["Subtask1", "Subtask2", "Subtask3"]
-  }]
-
-  let progress = {
-
-  }
-
-  let done ={
-
-  }
+let tasks = [
+    {
+        "taskTitle": "Erster Task",
+        "description": "Beschreibung des ersten Tasks",
+        "assignedTo": ["John", "Anna", "Emily"],
+        "dueDate": "15-01-2025",
+        "priority": "Urgent",
+        "category": "Work",
+        "subtasks": ["Subtask1", "Subtask2", "Subtask3"],
+        "state": "Pending"
+    },
+    {
+        "taskTitle": "Zweiter Task",
+        "description": "Beschreibung des zweiten Tasks",
+        "assignedTo": ["Alice", "Bob"],
+        "dueDate": "25-04-2023",
+        "priority": "Medium",
+        "category": "Personal",
+        "subtasks": ["SubtaskA", "SubtaskB"],
+        "state": "InProgress"
+    }
+];
