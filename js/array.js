7e3d834e91fb5178cbc7ea29276e395c429d9cdc
Marco Ammann <marco.ammann@hotmail.com>
2023-12-11 19:18:20 +0100

    created tasks example

---
+++ js/array.js
@@ -0,0 +1,9 @@
+let tasks = {
+    "taskTitle": "Titel des Tasks",
+    "description": "Beschreibung des Tasks",
+    "assignedTo": ["Kontakt1", "Kontakt2", "Kontakt3"],
+    "dueDate": "Fälligkeitsdatum",
+    "priority": "Urgent/Medium/Low",
+    "category": "Kategorie",
+    "subtasks": ["Subtask1", "Subtask2", "Subtask3"]
+  }
