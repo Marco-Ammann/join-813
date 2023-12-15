3e9216bb64d74dedae20c8396c8428b795b65afe
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2023-12-16 00:56:15 +0100

    contacts.js added renderContactsInfo functionality

--- js/contacts.js
+++ js/contacts.js
@@ -1,10 +1,13 @@
+/**
+ * Initializes the contacts.html
+ */
 function initContacts() {
     renderContactsList();
-    // TODO: New contacts must be pusht in sorted by the first letter of their name 
-
 }
 
-
+/**
+ * Renders the contact list sorted by the first name
+ */
 function renderContactsList() {
     let contactsList = document.getElementById('contactsList');
     contactsList.innerHTML = /* html */ `<div class="contacts-list-padding"></div>`;
@@ -24,7 +27,11 @@ function renderContactsList() {
     }
 }
 
-
+/**
+ * Returns the first letter of the first and last name
+ * @param {string} name 
+ * @returns - name initials
+ */
 function getInitials(name) {
     let firstLastName = name.split(' ');
     let firstLetter = firstLastName[0].charAt(0);
@@ -32,14 +39,30 @@ function getInitials(name) {
     return firstLetter + secondLetter;
 }
 
-
+/**
+ * Opens the clicked contact and deselects previous one if it exists
+ * @param {number} i 
+ */
 function openContact(i) {
+    renderContactsList();
     document.getElementById('contactsInfo').innerHTML = '';
+
+    let contact = contacts[i];
     document.getElementById(`contact${i}`).classList.add('contact-selected');
-    document.getElementById('contactsInfo').innerHTML = returnContactsInfoHTML();
+    document.getElementById('contactsInfo').innerHTML = returnContactsInfoHTML(contact['color'], getInitials(contact['name']), contact['name'], contact['email'], contact['phone']);
 }
 
-
+/**
+ * Return the requested HTML Code for the contact list
+ * @param {number} num 
+ * @param {string} letter 
+ * @param {number} i 
+ * @param {string} color 
+ * @param {string} initials 
+ * @param {string} name 
+ * @param {string} email 
+ * @returns - HTML Code as string
+ */
 function returnContactsListHTML(num, letter, i, color, initials, name, email) {
     if (num == 0) {
         return /* html */ `
@@ -59,12 +82,20 @@ function returnContactsListHTML(num, letter, i, color, initials, name, email) {
     }
 }
 
-
-function returnContactsInfoHTML() {
+/**
+ * Return HTML Code for the opened contact info
+ * @param {string} color 
+ * @param {string} initials 
+ * @param {string} name 
+ * @param {string} email 
+ * @param {string} phone 
+ * @returns - HTML Code as string
+ */
+function returnContactsInfoHTML(color, initials, name, email, phone) {
     return /* html */ `<div class="contacts-info-title">
-        <div class="contacts-user-icon icon-big" style="background-color: #FF7A00;">AM</div>
+        <div class="contacts-user-icon icon-big" style="background-color: ${color};">${initials}</div>
         <div class="contacts-info-name">
-            <h2>Anton Mayer</h2>
+            <h2>${name}</h2>
             <div class="contacts-info-buttons">
                 <div>
                     <img src="./assets/img/Desktop/general_elements/contacts/edit.svg" alt="Edit">
@@ -81,11 +112,11 @@ function returnContactsInfoHTML() {
     <div class="contacts-info-me">
         <div>
             <h4>Email</h4>
-            <a href="#">antom@gmail.com</a>
+            <a href="mailto:${email}">${email}</a>
         </div>
         <div>
             <h4>Phone</h4>
-            <span>+49 1111 111 11 1</span>
+            <span>${phone}</span>
         </div>
     </div>`;
 }
