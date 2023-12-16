bf77208e1e185adf23cd744632c51997c87daea7
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2023-12-16 18:15:45 +0100

    contacts.html/css created add new contact popup

--- js/contacts.js
+++ js/contacts.js
@@ -10,8 +10,8 @@ function initContacts() {
  * Renders the contact list sorted by the first name
  */
 function renderContactsList() {
-    let contactsList = document.getElementById('contactsList');
-    contactsList.innerHTML = /* html */ `<div class="contacts-list-padding"></div>`;
+    let contactList = document.getElementById('contactList');
+    contactList.innerHTML = /* html */ `<div class="contact-list-padding"></div>`;
 
     //TODO: Apply sort function directly at creating new contacts! 
     contacts.sort((a, b) => a.name.localeCompare(b.name));
@@ -22,13 +22,17 @@ function renderContactsList() {
         const firstLetter = element['name'].charAt(0);
 
         if (letters.indexOf(firstLetter) == -1) {
-            contactsList.innerHTML += returnContactsListHTML(0, firstLetter);
+            contactList.innerHTML += returnContactsListHTML(0, firstLetter);
             letters.push(firstLetter);
         }
-        contactsList.innerHTML += returnContactsListHTML(1, 0, i, element['color'], getInitials(element['name']), element['name'], element['email']);
+        contactList.innerHTML += returnContactsListHTML(1, 0, i, element['color'], getInitials(element['name']), element['name'], element['email']);
     }
 }
 
+
+function openAddNewContact() {
+
+}
 
 /**
  * Opens the clicked contact and deselects previous one if it exists
@@ -43,7 +47,10 @@ function openContact(i) {
     document.getElementById('contactsInfo').innerHTML = returnContactsInfoHTML(contact['color'], getInitials(contact['name']), contact['name'], contact['email'], contact['phone'], i);
 }
 
-
+/**
+ * Deletes the clicked contact and removes open contact info
+ * @param {number} i 
+ */
 function deleteContact(i) {
     contacts.splice(i, 1);
 
@@ -79,15 +86,15 @@ function getInitials(name) {
 function returnContactsListHTML(num, letter, i, color, initials, name, email) {
     if (num == 0) {
         return /* html */ `
-            <div class="contacts-letter">
+            <div class="letter">
                 <span>${letter}</span>
             </div>
-            <img class="contacts-border" src="./assets/img/Desktop/general_elements/bar/vector_gray_vertical.svg">`;
+            <img class="border" src="./assets/img/Desktop/general_elements/bar/vector_gray_vertical.svg">`;
     } else if (num == 1) {
         return /* html */ `
-            <div id="contact${i}" class="contacts-user" onclick="openContact(${i})">
-                <div class="contacts-user-icon" style="background-color: ${color};">${initials}</div>
-                <div class="contacts-username">
+            <div id="contact${i}" class="user" onclick="openContact(${i})">
+                <div class="user-icon" style="background-color: ${color};">${initials}</div>
+                <div class="username">
                     <span>${name}</span>
                     <a href="mailto:${email}">${email}</a>
                 </div>
@@ -102,28 +109,29 @@ function returnContactsListHTML(num, letter, i, color, initials, name, email) {
  * @param {string} initials 
  * @param {string} name 
  * @param {string} email 
- * @param {string} phone 
+ * @param {string} phone
+ * @param {number} i 
  * @returns - HTML Code as string
  */
 function returnContactsInfoHTML(color, initials, name, email, phone, i) {
-    return /* html */ `<div class="contacts-info-title">
-        <div class="contacts-user-icon icon-big" style="background-color: ${color};">${initials}</div>
-        <div class="contacts-info-name">
+    return /* html */ `<div class="info-title">
+        <div class="user-icon icon-big" style="background-color: ${color};">${initials}</div>
+        <div class="info-name">
             <h2>${name}</h2>
-            <div class="contacts-info-buttons">
+            <div class="info-buttons">
                 <div>
-                    <img src="./assets/img/Desktop/general_elements/contacts/edit.svg" alt="Edit">
+                    <img src="./assets/img/Desktop/contacts/edit.svg" alt="Edit">
                     <span>Edit</span>
                 </div>
                 <div onclick="deleteContact(${i})">
-                    <img src="./assets/img/Desktop/general_elements/contacts/delete.svg" alt="Delete">
+                    <img src="./assets/img/Desktop/contacts/delete.svg" alt="Delete">
                     <span>Delete</span>
                 </div>
             </div>
         </div>
     </div>
     <h6>Contact Information</h6>
-    <div class="contacts-info-me">
+    <div class="info-me">
         <div>
             <h4>Email</h4>
             <a href="mailto:${email}">${email}</a>
