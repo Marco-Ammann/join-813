5fe1c7f4c663279b1316b7f481f100f95e43872f
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2023-12-16 15:39:07 +0100

    contacts.js added delete function

--- js/contacts.js
+++ js/contacts.js
@@ -5,6 +5,7 @@ function initContacts() {
     renderContactsList();
 }
 
+
 /**
  * Renders the contact list sorted by the first name
  */
@@ -12,6 +13,7 @@ function renderContactsList() {
     let contactsList = document.getElementById('contactsList');
     contactsList.innerHTML = /* html */ `<div class="contacts-list-padding"></div>`;
 
+    //TODO: Apply sort function directly at creating new contacts! 
     contacts.sort((a, b) => a.name.localeCompare(b.name));
     let letters = [];
 
@@ -27,6 +29,29 @@ function renderContactsList() {
     }
 }
 
+
+/**
+ * Opens the clicked contact and deselects previous one if it exists
+ * @param {number} i 
+ */
+function openContact(i) {
+    renderContactsList();
+    document.getElementById('contactsInfo').innerHTML = '';
+
+    let contact = contacts[i];
+    document.getElementById(`contact${i}`).classList.add('contact-selected');
+    document.getElementById('contactsInfo').innerHTML = returnContactsInfoHTML(contact['color'], getInitials(contact['name']), contact['name'], contact['email'], contact['phone'], i);
+}
+
+
+function deleteContact(i) {
+    contacts.splice(i, 1);
+
+    renderContactsList();
+    document.getElementById('contactsInfo').innerHTML = '';
+}
+
+
 /**
  * Returns the first letter of the first and last name
  * @param {string} name 
@@ -39,18 +64,6 @@ function getInitials(name) {
     return firstLetter + secondLetter;
 }
 
-/**
- * Opens the clicked contact and deselects previous one if it exists
- * @param {number} i 
- */
-function openContact(i) {
-    renderContactsList();
-    document.getElementById('contactsInfo').innerHTML = '';
-
-    let contact = contacts[i];
-    document.getElementById(`contact${i}`).classList.add('contact-selected');
-    document.getElementById('contactsInfo').innerHTML = returnContactsInfoHTML(contact['color'], getInitials(contact['name']), contact['name'], contact['email'], contact['phone']);
-}
 
 /**
  * Return the requested HTML Code for the contact list
@@ -82,6 +95,7 @@ function returnContactsListHTML(num, letter, i, color, initials, name, email) {
     }
 }
 
+
 /**
  * Return HTML Code for the opened contact info
  * @param {string} color 
@@ -91,7 +105,7 @@ function returnContactsListHTML(num, letter, i, color, initials, name, email) {
  * @param {string} phone 
  * @returns - HTML Code as string
  */
-function returnContactsInfoHTML(color, initials, name, email, phone) {
+function returnContactsInfoHTML(color, initials, name, email, phone, i) {
     return /* html */ `<div class="contacts-info-title">
         <div class="contacts-user-icon icon-big" style="background-color: ${color};">${initials}</div>
         <div class="contacts-info-name">
@@ -101,7 +115,7 @@ function returnContactsInfoHTML(color, initials, name, email, phone) {
                     <img src="./assets/img/Desktop/general_elements/contacts/edit.svg" alt="Edit">
                     <span>Edit</span>
                 </div>
-                <div>
+                <div onclick="deleteContact(${i})">
                     <img src="./assets/img/Desktop/general_elements/contacts/delete.svg" alt="Delete">
                     <span>Delete</span>
                 </div>
