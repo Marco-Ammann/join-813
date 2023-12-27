d2651d860f993c38b783c7ae6b4bbecbca7635ce
Marco Ammann <marco.ammann@hotmail.com>
2023-12-27 01:57:35 +0100

    adjusted edit card

---
+++ js/edit_task.js
@@ -0,0 +1,26 @@
+
+function getTitleValue(i) {
+    const cardTitleElement = document.getElementById(`openCard`).querySelector('h3');
+    return cardTitleElement.textContent; 
+}
+
+function getDescriptionValue(i) {
+    const cardDescriptionElement = document.getElementById(`openCard`).querySelector('p');
+    return cardDescriptionElement.textContent;
+}
+
+
+
+
+function getValuesToEdit() {
+    //logik zum holen der values welche bereits gesetzt sind
+}
+
+
+
+
+
+function setValues() {
+    //logik zum einsetzen der values
+}
+
