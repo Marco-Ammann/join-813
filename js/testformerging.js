9f50bed2978b83b7438c824f7d99971cfcf9e122
Marco Ammann <marco.ammann@hotmail.com>
2023-12-27 02:13:19 +0100

    merge test 3

--- js/testformerging.js
+++ js/testformerging.js
@@ -5,28 +5,3 @@
 5
 6
 7
-8
-9
-0
-'
-'
-56
-ReportingObserverrgbf
-gbfgb
-fgbf
-blurfgb
-fgbffgb
-gfgf
-ksdflsdlkfj
-kjdfjldskj
-lkdfjskjf
-8044
-9438
-blurfgbfg
-blurfgbbg
-blurfgbbgf
-blurfgb
-'babfb jerkvnkenjkujnfir
-fwrefg ergerg
-egeart
-generateAssignContacts evalgeag
