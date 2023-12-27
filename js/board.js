f49e0d51791fca283e0fb400d0c73924e514db81
Marco Ammann <marco.ammann@hotmail.com>
2023-12-27 01:07:43 +0100

    added editCard function to the task card

--- js/board.js
+++ js/board.js
@@ -115,7 +115,7 @@ function openCard(i) {
                 <path d="M1 0V24" stroke="#D1D1D1"/>
                 </svg>
                 </div>
-            <a href="#">
+            <a href="#" onclick="editCard()">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <mask id="mask0_118031_4276" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
                 <rect width="24" height="24" fill="#D9D9D9"/>
