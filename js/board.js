db772eab8258e88ecc76f63a8cf30788d55374a6
Marco Ammann <marco.ammann@hotmail.com>
2024-01-03 22:10:49 +0100

    adjusted css

--- js/board.js
+++ js/board.js
@@ -152,8 +152,8 @@ async function editCard(taskIndex) {
     await loadContacts();
     await generateAssignContacts('assignDropdown-popup', 'assigned-contacts-popup');
 
-    setupDropdownCloseListener('assignDropdown-popup', 'add-contact-input', 'arrowAssign-popup');
-    setupFilterListener('add-contact-input', 'assignDropdown-popup');
+    setupDropdownCloseListener('assignDropdown-popup', 'add-contact-input', 'arrowAssign');
+    setupFilterListener('add-contact-input-popup', 'assignDropdown-popup');
 
     toggleDropdown('assignDropdown-popup', 'add-contact-input-popup', 'arrowAssign-popup', 'Select contacts to assign');
     setClickedContacts(taskIndex, 'assigned-contacts-popup');
