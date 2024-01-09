d3ef8a67b19dd81b660a7c0d592c32f9b11ce43c
Marco Ammann <46301477+Melendros@users.noreply.github.com>
2024-01-09 22:17:23 +0100

    enabled search function to find title and description

--- js/board_helpfunctions.js
+++ js/board_helpfunctions.js
@@ -7,9 +7,8 @@ let taskFound = false;
  * @returns {string} A formatted category class name.
  */
 function category(text) {
-  return "header" + text.replace(/\s/g, "");
+   return 'header' + text.replace(/\s/g, '');
 }
-
 
 /**
  * Truncates the text to a specified length, ending at the last complete word within the limit.
@@ -19,20 +18,19 @@ function category(text) {
  * @returns {string} Truncated text ending with an ellipsis if it exceeds the specified length.
  */
 function addDescription(text, maxLength = 40) {
-  if (text.length <= maxLength) {
-    return text;
-  }
+   if (text.length <= maxLength) {
+      return text;
+   }
 
-  let truncatedText = text.substring(0, maxLength);
-  const lastSpaceIndex = truncatedText.lastIndexOf(" ");
+   let truncatedText = text.substring(0, maxLength);
+   const lastSpaceIndex = truncatedText.lastIndexOf(' ');
 
-  if (lastSpaceIndex !== -1) {
-    truncatedText = truncatedText.substring(0, lastSpaceIndex);
-  }
+   if (lastSpaceIndex !== -1) {
+      truncatedText = truncatedText.substring(0, lastSpaceIndex);
+   }
 
-  return truncatedText + "...";
+   return truncatedText + '...';
 }
-
 
 /**
  * Adds a progress bar to a task card based on the completion of subtasks.
@@ -40,20 +38,19 @@ function addDescription(text, maxLength = 40) {
  * @param {number} i - The index of the task in the tasks array.
  */
 function addProgressBar(i) {
-  let task = tasks[i]["subtasks"].length + tasks[i]["subtasksDone"].length;
-  if (task > 0) {
-    let calculatetSubtask = 100 / task;
-    calculatetSubtask = calculatetSubtask * tasks[i]["subtasksDone"].length;
-    content = document.getElementById(`progressbar${i}`);
-    content.innerHTML = /*html*/ `
+   let task = tasks[i]['subtasks'].length + tasks[i]['subtasksDone'].length;
+   if (task > 0) {
+      let calculatetSubtask = 100 / task;
+      calculatetSubtask = calculatetSubtask * tasks[i]['subtasksDone'].length;
+      content = document.getElementById(`progressbar${i}`);
+      content.innerHTML = /*html*/ `
                 <progress max="100" value="${calculatetSubtask}"></progress>
-                <span>${tasks[i]["subtasksDone"].length}/${task} Subtask</span>
+                <span>${tasks[i]['subtasksDone'].length}/${task} Subtask</span>
                 `;
-  } else {
-    document.getElementById(`progressbar${i}`).remove();
-  }
+   } else {
+      document.getElementById(`progressbar${i}`).remove();
+   }
 }
-
 
 /**
  * Sets the priority of the task to match the specified priority.
@@ -61,10 +58,9 @@ function addProgressBar(i) {
  * @param {number} taskIndex - The index of the task in the tasks array.
  */
 function clickPriority(taskIndex) {
-  let setPriority = tasks[taskIndex].priority;
-  setPrio(setPriority);
+   let setPriority = tasks[taskIndex].priority;
+   setPrio(setPriority);
 }
-
 
 /**
  * Adds icons for assigned contacts to the open card.
@@ -73,15 +69,14 @@ function clickPriority(taskIndex) {
  * @param {number} x - The index of the task in the tasks array.
  */
 function addOpenTaskIcon(id, x) {
-  let content = document.getElementById(id);
-  tasks[x]["assignedTo"].forEach((assignedContact) => {
-    let contact = contacts.find((c) => c.id === assignedContact.id);
-    if (contact) {
-      content.innerHTML += createContactIcon(contact);
-    }
-  });
+   let content = document.getElementById(id);
+   tasks[x]['assignedTo'].forEach((assignedContact) => {
+      let contact = contacts.find((c) => c.id === assignedContact.id);
+      if (contact) {
+         content.innerHTML += createContactIcon(contact);
+      }
+   });
 }
-
 
 /**
  * Adds icons for assigned contacts to a task card.
@@ -90,36 +85,34 @@ function addOpenTaskIcon(id, x) {
  * @param {number} x - The index of the task in the tasks array.
  */
 function addTaskIcon(id, x) {
-  let content = document.getElementById(id);
-  let contactsLeft;
-  const assignedContacts = tasks[x]["assignedTo"];
+   let content = document.getElementById(id);
+   let contactsLeft;
+   const assignedContacts = tasks[x]['assignedTo'];
 
-  id = id.slice(-1);
+   id = id.slice(-1);
 
-  for (let i = 0; i < assignedContacts.length; i++) {
-    let contact = contacts.find((c) => c.id === assignedContacts[i].id)
-    contactsLeft = assignedContacts.length - i;
-    content.innerHTML += createContactIconHTML(contact, i, contactsLeft);
+   for (let i = 0; i < assignedContacts.length; i++) {
+      let contact = contacts.find((c) => c.id === assignedContacts[i].id);
+      contactsLeft = assignedContacts.length - i;
+      content.innerHTML += createContactIconHTML(contact, i, contactsLeft);
 
-    if (i == 3) {
-      return;
-    }
-  }
+      if (i == 3) {
+         return;
+      }
+   }
 }
-
 
 /**
  * Adds a transition effect to show the open task card.
  */
 function addTransition() {
-  const openCardContainer = document.getElementById("openCardContainer");
-  const openCard = document.getElementById("openCard");
+   const openCardContainer = document.getElementById('openCardContainer');
+   const openCard = document.getElementById('openCard');
 
-  openCardContainer.classList.remove("hidden");
-  openCardContainer.style.animation = "blendIn 100ms ease-out forwards";
-  openCard.style.animation = "slideInCard 100ms ease-out forwards";
+   openCardContainer.classList.remove('hidden');
+   openCardContainer.style.animation = 'blendIn 100ms ease-out forwards';
+   openCard.style.animation = 'slideInCard 100ms ease-out forwards';
 }
-
 
 /**
  * Initiates the drag operation when an element is being dragged.
@@ -127,9 +120,8 @@ function addTransition() {
  * @param {number} id - The unique identifier of the element being dragged.
  */
 function startDraggin(id) {
-  currentDraggedElement = id;
+   currentDraggedElement = id;
 }
-
 
 /**
  * Allows a drop operation by preventing the default behavior of the dragover event.
@@ -137,9 +129,8 @@ function startDraggin(id) {
  * @param {Event} ev - The dragover event.
  */
 function allowDrop(ev) {
-  ev.preventDefault();
+   ev.preventDefault();
 }
-
 
 /**
  * Moves a task to a different category.
@@ -148,19 +139,18 @@ function allowDrop(ev) {
  * @param {number} id - The ID of the task to be moved.
  */
 async function moveTo(category) {
-  tasks[currentDraggedElement][`state`] = category;
-  await saveStateChangeAfterDroppingTask();
-  renderTasks();
-  removeHighlight();
+   tasks[currentDraggedElement][`state`] = category;
+   await saveStateChangeAfterDroppingTask();
+   renderTasks();
+   removeHighlight();
 }
-
 
 /**
  * Saves Tasks into Backend
  */
 async function saveStateChangeAfterDroppingTask() {
-  let tasksToPush = tasks;
-  await setItem("tasks", tasksToPush);
+   let tasksToPush = tasks;
+   await setItem('tasks', tasksToPush);
 }
 
 /**
@@ -169,15 +159,14 @@ async function saveStateChangeAfterDroppingTask() {
  * @param {Array} tasks - An array of tasks to be checked.
  */
 function checkAndAddTasks(tasks) {
-  states.forEach((state) => {
-    const filteredTasks = tasks.filter((task) => task.state === state);
+   states.forEach((state) => {
+      const filteredTasks = tasks.filter((task) => task.state === state);
 
-    if (filteredTasks.length === 0) {
-      addNoTaskHTML(state + "Container");
-    }
-  });
+      if (filteredTasks.length === 0) {
+         addNoTaskHTML(state + 'Container');
+      }
+   });
 }
-
 
 /**
  * Sorts and filters task cards based on a given search term. It searches within card titles and
@@ -186,43 +175,51 @@ function checkAndAddTasks(tasks) {
  * @param {string} inputId - The ID of the input element containing the search term.
  */
 function sortAndFilterCards(inputId) {
-  document.getElementById('allertNoTasksFoundContainer').innerHTML = '';
-  const searchTerm = document.getElementById(inputId).value.toLowerCase();
-  const cards = document.querySelectorAll(".toDoCard");
-  const states = [
-    "ToDo",
-    "InProgress",
-    "Done",
-    "AwaitFeedback",
-  ];
+   document.getElementById('allertNoTasksFoundContainer').innerHTML = '';
+   const searchTerm = document.getElementById(inputId).value.toLowerCase();
+   const cards = document.querySelectorAll('.toDoCard');
+   const states = ['ToDo', 'InProgress', 'Done', 'AwaitFeedback'];
 
-  if (searchTerm) {
-    states.forEach((state) => {
-      removeNoTaskHTML(state);
-    });
-  } else if (!searchTerm) {
-    checkAndAddTasks(tasks);
-  }
+   if (searchTerm) {
+      states.forEach((state) => {
+         removeNoTaskHTML(state);
+      });
+   } else if (!searchTerm) {
+      checkAndAddTasks(tasks);
+   }
 
-  cards.forEach((card) => {
-    const title = card.querySelector("h3").textContent.toLowerCase();
-    if (title.includes(searchTerm)) {
-      card.parentNode.style.display = "block";
-      taskFound = true;
+   cards.forEach((card) => {
+      const title = card.querySelector('h3').textContent.toLowerCase();
+      if (title.includes(searchTerm)) {
+         card.parentNode.style.display = 'block';
+         taskFound = true;
+      } else {
+         card.parentNode.style.display = 'none';
+      }
+   });
+
+   cards.forEach((card) => {
+    const description = card.querySelector('p').textContent.toLowerCase();
+    if (description.includes(searchTerm)) {
+       card.parentNode.style.display = 'block';
+       taskFound = true;
     } else {
-      card.parentNode.style.display = "none";
+       card.parentNode.style.display = 'none';
     }
-  });
+ });
 
-  if (taskFound === false) {
-    let content = document.getElementById('allertNoTasksFoundContainer');
-    content.innerHTML = 'No task found';
-    document.getElementById('allertNoTasksFoundContainer').style.zIndex = "10";
-  }
-  taskFound = false;
+   if (taskFound === false) {
+      let content = document.getElementById('allertNoTasksFoundContainer');
+      content.innerHTML = 'No task found';
+      document.getElementById('allertNoTasksFoundContainer').style.zIndex = '10';
+   }
+   taskFound = false;
 }
 
+
+
+
 function RemoveNoTaksFound() {
-  console.log('test');
-  document.getElementById('allertNoTasksFoundContainer').innerHTML = '';
+   console.log('test');
+   document.getElementById('allertNoTasksFoundContainer').innerHTML = '';
 }
